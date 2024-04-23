eac474621aade0d2bccf6be4e7a241ba73de0cba
Codewithabrar <162000914+Codewithabrar@users.noreply.github.com>
2024-04-23 22:39:09 +0500

    make your list

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestion = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "What would you like to add in your todos ?"
+        },
+        {
+            name: "nextQuestion",
+            type: "confirm",
+            message: "would you add more",
+            default: "true"
+        }
+    ]);
+    todos.push(todoQuestion.firstQuestion);
+    condition = todoQuestion.nextQuestion;
+    console.log(todos);
+}
